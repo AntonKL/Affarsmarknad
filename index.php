15d013ee90c3e4f7119753e503d8056478c6e456
Anton Kjörck Lindén <anton@noviumdesign.se>
2013-10-08 10:45:13 +0200

    Added some demo content

--- index.php
+++ index.php
@@ -8,59 +8,108 @@
 		<meta name="description" content="">
 		<meta name="keywords" content="">
 
-		<link href="assets/css/bootstrap.css" rel="stylesheet">
 		<link href="assets/css/style.css" rel="stylesheet">
 	</head>
-	<body class="home">
+	<body class="home page">
 
 		<?php include 'nav.html'; ?>
-
-		<div id="company-of-the-day" class="carousel slide">
-			<div class="carousel-inner">
-				<div class="active item slide1">
-					<div class="container">
-						<div class="hero-unit highlighted-company">
-							<h2>DigiBook</h2>
-							<p>Företaget som låter dig digitalisera alla dina skolböcker. Glöm tunga skolväskor - allt får plats i mobilen, datorn eller läsplattan. Syncas automatiskt i molnet!</p>
-							<p><a class="btn btn-success">Mer om dagens företag &raquo;</a></p>
+		<div class="container">
+			<div class="row">
+				<div class="companygrid">
+					<div class="span2">
+						<div>
+							<div class="body">
+								<img src="assets/companys/novium.png">
+								<h5>Novium Designbyrå</h5>
+								<p>
+									Vi hjälper företag att synas i mängden och bli valda.
+								</p>
+							</div>
+							<div class="footer">
+								<div class="social">
+									<a class="btn btn-small" href="#"><i class="icon-thumbs-up"></i> Gilla</a>
+									<a class="btn btn-small" href="#"><i class="icon-retweet"></i> Tweeta</a>
+								</div>
+								<ul class="unstyled">
+									<li><i class="icon-arrow-up"></i> Aktiekurs +4,9%</li>
+									<li><i class="icon-list-alt"></i> Ny pressrelese</li>
+									<li><i class="icon-ok-circle"></i> Uppdaterad profil</li>
+									<li><i class="icon-briefcase"></i> IT, media & hemelektronik</li>
+									<li><i class="icon-tags"></i> Taggar:</li>
+									<li>
+										<span class="label label-info">Branding</span>
+										<span class="label label-info">Webbdesign</span>
+										<span class="label label-info">Logotyper</span>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="span3">
+						<div>
+							<div class="body">
+								<img src="assets/companys/novium.png">
+								<h5>Novium Designbyrå</h5>
+								<p>
+									Vi hjälper företag att synas i mängden och bli valda.
+								</p>
+							</div>
+							<div class="footer">
+								<div class="social">
+									<a class="btn btn-small" href="#"><i class="icon-thumbs-up"></i> Gilla</a>
+									<a class="btn btn-small" href="#"><i class="icon-retweet"></i> Tweeta</a>
+								</div>
+								<ul class="unstyled">
+									<li><i class="icon-arrow-up"></i> Aktiekurs +4,9%</li>
+									<li><i class="icon-list-alt"></i> Ny pressrelese</li>
+									<li><i class="icon-ok-circle"></i> Uppdaterad profil</li>
+									<li><i class="icon-briefcase"></i> IT, media & hemelektronik</li>
+									<li><i class="icon-tags"></i> Taggar:</li>
+									<li>
+										<span class="label label-info">Branding</span>
+										<span class="label label-info">Webbdesign</span>
+										<span class="label label-info">Logotyper</span>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					</div>
+					<div class="span2">
+						<div>
+							content
+						</div>
+					</div>
+					<div class="span2">
+						<div>
+							content
+						</div>
+					</div>
+					<div class="span2">
+						<div>
+							content
+						</div>
+					</div>
+					<div class="span2">
+						<div>
+							content
+						</div>
+					</div>
+					<div class="span2">
+						<div>
+							content
+						</div>
+					</div>
+					<div class="span2">
+						<div>
+							content
 						</div>
 					</div>
 				</div>
-				<div class="item">Bild nummer två i bildspelet</div>
-				<div class="item">Bild nummer tre i bildspelet</div>
-			</div>
-			<a class="carousel-control left" href="#company-of-the-day" data-slide="prev">&lsaquo;</a>
-			<a class="carousel-control right" href="#company-of-the-day" data-slide="next">&rsaquo;</a>
-		</div>
-		<div class="be-a-member align-center"><a href="#"><i>Vill ert företag också synas här?</i></a></div>
-
-		<div class="container">
-			<div class="introduction">
-
-				<h1>Välkommen till UF-marknad</h1>
-				<p class="marketing-byline">Låt oss presentera möjligheterna med vår tjänst!</p>
-
-				<div class="row">
-					<div class="span4">
-						<img src="assets/img/icon1.png">
-						<h3>Sök företag</h3>
-						<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-					</div>
-					<i class="icon1"></i>
-					<div class="span4">
-						<img src="assets/img/icon1.png">
-						<h3>Hitta spännande produkter</h3>
-						<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-					</div>
-					<i class="icon1"></i>
-					<div class="span4">
-						<img src="assets/img/icon1.png">
-						<h3>Anlita unga entreprenörer</h3>
-						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-					</div>
-				</div>
 			</div>
 		</div>
+
+		
 
 		<?php include 'footer.html'; ?>
 
@@ -74,7 +123,7 @@
 
 				<form class="form-horizontal">
 					<div class="control-group">
-						<label class="control-label" for="inputEmail">Org. nr. UF</label>
+						<label class="control-label" for="inputEmail">Org. nr.</label>
 						<div class="controls">
 							<input type="text" id="inputRegnr" placeholder="Organisationsnummer">
 						</div>
